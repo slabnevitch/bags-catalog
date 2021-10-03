c8179aaf9f95247a626f8e7c8f8fa6c6ca977467
slabnevitch <handofschpilrain@mail.ru>
2021-10-03 14:29:09 +0500

    check path prefix

--- _nuxt/static/1633248019/270/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fbags-catalog\u002F_nuxt\u002Fstatic\u002F1633248019",layout:"default",error:a,serverRendered:true,routePath:"\u002F270",config:{_app:{basePath:"\u002Fbags-catalog\u002F",assetsPath:"\u002Fbags-catalog\u002F_nuxt\u002F",cdnURL:a}}}}(null));
